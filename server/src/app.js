c9e7fad297e9ceced975b710b229bd53a4143434
Martin Metodiev <martin.metodiew@gmail.com>
2025-06-04 21:27:36 +0300

    feat(server): add HTTP request logging with morgan middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const config = require('./config');
 const api = require('./routes/api');
@@ -9,6 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({ origin: config.corsOrigin }));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
