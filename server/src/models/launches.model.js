1ce043b26acf91807014c490cc7e20d7da0cf497
Martin Metodiev <martin.metodiew@gmail.com>
2025-06-04 21:41:27 +0300

    refactor(server): restructure data access layer with model functions

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,7 +1,22 @@
-const launches = [];
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: 'Kepler Exploration X',
+  rocket: 'Explorer IS1',
+  launchDate: new Date('December 27, 2030'),
+  target: 'Kepler-442 b',
+  customers: ['Martin Metodiev', 'NASA'],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+async function getAllLaunches() {
+  return Array.from(launches.values());
+}
 
 module.exports = {
-  getAllLaunches: async () => {
-    return { launches };
-  },
+  getAllLaunches,
 };
