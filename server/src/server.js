896ef9156fbd7f4ea0e59544030d2739b9cca4e1
Martin Metodiev <martin.metodiew@gmail.com>
2025-06-07 21:26:53 +0300

    feat(server): add a MongoDB connection via mongoose service

--- server/src/server.js
+++ server/src/server.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 
 const app = require('./app');
 
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
---
+++ server/src/services/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error('Missing config: MONGO_URI not provided in .env');
+}
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URI);
+}
+ 
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
